295d8f77d6a16079263a76df7919f6e4ea5ecfca
Josh Baisc <joshbawesome@hotmail.com>
2019-03-20 23:19:04 -0700

    changed bot activity

--- app.js
+++ app.js
@@ -17,26 +17,26 @@ const config = require("./config.json");
 const YTDL = require("ytdl-core");
 
 
-
+var varActivity = "Trolling";
 client.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setActivity(`on ${client.guilds.size} servers`);
+  client.user.setActivity(varActivity);
 });
 
 client.on("guildCreate", guild => {
 	
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setActivity(`on ${client.guilds.size} servers`);
+  client.user.setActivity(varActivity);
 });
 
 client.on("guildDelete", guild => {
   // this event triggers when the bot is removed from a guild.
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`on ${client.guilds.size} servers`);
+  client.user.setActivity(varActivity);
 });
 
 
